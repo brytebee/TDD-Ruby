5826e077d5b93a86bbf2b243912d172705eca459
brytebee <brytebee@gmail.com>
2022-04-14 21:00:01 +0100

    :white_check_mark: Add factorial
    Added factorial

--- RSpec/lib/solver.rb
+++ RSpec/lib/solver.rb
@@ -1,2 +1,15 @@
 class Solver
+  def factorial(num)
+    raise 'Use 0 or positive integers only' if num.negative? || !num.is_a?(Integer)
+
+    factorial = 1
+    i = 1
+
+    while i <= num
+      factorial = i * factorial
+      i += 1
+    end
+
+    factorial
+  end
 end
