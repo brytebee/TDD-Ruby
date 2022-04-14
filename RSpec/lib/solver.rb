e457ca6af86d61c87e446eaac9d339ebd27f0c41
brytebee <brytebee@gmail.com>
2022-04-14 21:24:05 +0100

    :recycle: Refactor
    Refactored

--- RSpec/lib/solver.rb
+++ RSpec/lib/solver.rb
@@ -1,28 +1,15 @@
 class Solver
   def factorial(num)
     raise 'Use 0 or positive integers only' if num.negative? || !num.is_a?(Integer)
-
-    factorial = 1
-    i = 1
-
-    while i <= num
-      factorial = i * factorial
-      i += 1
-    end
-
-    factorial
+    return 1 if num == 0
+    return factorial(num - 1) * num
   end
 
   def reverse(str)
     str.reverse
   end
 
-  def fizzbuzz(number)
-    if (number % 5).zero? && (number % 3).zero? then 'fizzbuzz'
-    elsif (number % 3).zero? then 'fizz'
-    elsif (number % 5).zero? then 'buzz'
-    else
-      number.to_s
-    end
+  def fizzbuzz(n)
+    (n % 15).zero? ? 'fizzbuzz' : (n % 3).zero? ? 'fizz' : (n % 5).zero? ? 'buzz' : "#{n}"
   end
 end
