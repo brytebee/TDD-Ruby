9a6da1dc8636ab56c2237d1b4558873f6328e222
brytebee <brytebee@gmail.com>
2022-04-14 20:58:49 +0100

    :white_check_mark: Created the factorial test
    Created the factorial test

--- RSpec/spec/solver_spec.rb
+++ RSpec/spec/solver_spec.rb
@@ -1,5 +1,15 @@
 require './solver'
 
 describe Solver do
-  
+  context 'When call the method factorial' do
+    it 'should return the factorial for the integer when using an integer > 0 as the argument' do
+      solver = Solver.new
+      factorial1 = solver.factorial(5)
+      factorial2 = solver.factorial(2)
+      factorial3 = solver.factorial(9)
+      expect(factorial1).to eq 120
+      expect(factorial2).to eq 2
+      expect(factorial3).to eq 362_880
+    end
+  end
 end
